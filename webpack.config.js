9ed236d236358943c65a3bc6087d854a3a508c67
Nithya <tryph101@hotmail.com>
2022-06-30 03:22:34 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+ "mode": "none",
+ "entry": "./src/index.js",
+ "output": {
+   "path": __dirname + '/dist',
+   "filename": "bundle.js"
+ },
+ devServer: {
+   contentBase: path.join(__dirname, 'dist')
+ },
+ "module": {
+   "rules": [
+     {
+       "test": /\.css$/,
+       "use": [
+         "style-loader",
+         "css-loader"
+       ]
+     },
+     {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+       filename: 'images/[name][ext]'
+    }
+      },
+     {
+       "test": /\.js$/,
+       "exclude": /node_modules/,
+       "use": {
+         "loader": "babel-loader",
+         "options": {
+           "presets": [
+             "@babel/preset-env",
+           ]
+         }
+       }
+     },
+   ]
+ }
+};
